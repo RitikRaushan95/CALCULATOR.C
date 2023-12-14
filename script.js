3a94ed6991c9bdc3579dbf9d77f64f15d5dae328
RITIK RAUSHAN <121702960+RitikRaushan95@users.noreply.github.com>
2023-12-14 23:34:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let y=document.getElementById('display');//the value shown in the display
+function appendToDisplay(value) {
+   y.value += value;
+}
+
+function clearDisplay() {
+    y.value = '';
+}
+
+function calculate() {
+
+    try {
+        y.value = eval(y.value);
+    } catch (error) {
+        y.value = 'Error';
+        y.style.color='red'
+    }
+}
+
+
+const heading= document.getElementsByClassName("blink");
+    let r=Math.floor(Math.random() * 255);
+    let g=Math.floor(Math.random() * 255);
+    let b=Math.floor(Math.random() * 255);
+
+    heading.style.backgroundColor  = `rgb(${r},${g},${b})`;
